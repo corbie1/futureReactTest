460ec80f63f0a62b944965c4983cf08ec59f5390
corbie1 <bratchikovjohn@gmail.com>
2020-09-15 14:19:10 +0300

    added github pages

---
+++ build/precache-manifest.cd5ac1f705f831aab87abab9ee06c888.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f3fef6566ad87764ffedfa67ce8e70b",
+    "url": "/futureReactTest/index.html"
+  },
+  {
+    "revision": "15ae442a7c9061dacd2c",
+    "url": "/futureReactTest/static/css/main.146b9cc1.chunk.css"
+  },
+  {
+    "revision": "663bb9a998e3185ad4f6",
+    "url": "/futureReactTest/static/js/2.641b2a07.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/futureReactTest/static/js/2.641b2a07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15ae442a7c9061dacd2c",
+    "url": "/futureReactTest/static/js/main.0bc4fb8c.chunk.js"
+  },
+  {
+    "revision": "bba7827e14a2fddc95b4",
+    "url": "/futureReactTest/static/js/runtime-main.3f0a5d42.js"
+  }
+]);
